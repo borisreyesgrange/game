a077593717b74da5621fadad78e3138828e40b6c
Boris Reyes <38927619+borisreyesgrange@users.noreply.github.com>
2018-06-05 17:29:01 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,11 +1,18 @@
+import time
 from functions import *
+graphics_color = random.choice(["red", "green", "yellow", "blue", "cyan"])
+game_color = "magenta"
+
+print(title(graphics_color))
+input(text_color(game_color, "Press enter when you are ready"))
+select_character(game_color)
+
+#print_title()
 
 #print(red("Mi texto"))
 
-#print(text_color("red", "Texto en rojo"))
+#print(text_color("cyan", "Texto a colorear"))
 
 #print(text_color("red", title()))
 
-#print(text_color("yellow", text(8, 12)))
-
-#print(text_color("yellow", text_delay(8, 12, 4)))
+#print(text_delay(8, 12, 4))
